b70a175fefcf1be00b63abd3b7a9532ae6c8dfdb
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-10-24 21:51:19 -0300

    segundo metodo passando como parametro
    Metodo via map

--- passarFuncaoComoParametro.js
+++ passarFuncaoComoParametro.js
@@ -14,3 +14,19 @@ function exibe_mensagem(funcao_mensagem) {
 }
 
 exibe_mensagem(mensagem_usuario);
+
+// Outra forma de passar funções como parametro
+
+let vetor = [1, 2, 3, 4, 5];
+
+function duplicar(num) {
+    console.log(num * 2);
+}
+
+function adiciona_10(num) {
+    console.log(num + 10);
+}
+
+vetor.map(duplicar); 
+vetor.map(adiciona_10);
+// map vai ser responsável por pegar cada elemento do veotr executar a função individual de cada um.
